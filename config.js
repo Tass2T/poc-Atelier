a1eece44cc439946580347126673825a280df4d7
tass2t <brunog90@gmail.com>
2023-01-30 18:04:53 +0100

    added colors to config

--- config.js
+++ config.js
@@ -5,4 +5,13 @@ export const config = {
   PEARL_NUMBERS: 150,
   PEARL_RADIUS: 350,
   ROTATION_SPEED: 0.00001,
+
+  ATELIER_COLORS: [
+    // dark blue
+    [0, 103, 255],
+    // green
+    [5, 235, 123],
+    // light blue
+    [0, 103, 255],
+  ],
 };
